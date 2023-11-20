6734acdc7bd1b7b0ee5f05e8b5f692499dec2a19
GabrielVictor <81785037+yNephtune@users.noreply.github.com>
2023-11-20 18:19:56 -0300

    algumas att
    subi o codigo sem o git msm na força bruta.

---
+++ Js/navbar.js
@@ -0,0 +1,12 @@
+class MobileNavbar{
+    constructor(mobileMenu, navLinks, navList){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass ="active"
+
+    }
+    addClickEvent(){
+        
+    }
+}
